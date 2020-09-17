469058f0ebe1f307c3ad2c025e2009c60673c82d
Amirrr1987 <Maghami.A1987@gmail.com>
2020-09-17 13:21:04 +0430

    create education component & import tags

---
+++ src/components/Education.vue
@@ -0,0 +1,50 @@
+<template>
+  <section class="education">
+    <div class="education__container">
+      <table class="education__table table">
+        <thead class="table__thead thead">
+          <tr class="thead__row">
+            <th class="thead__col thead--title" colspan="4">اطلاعات پایه :</th>
+          </tr>
+          <tr class="thead__row">
+            <th class="thead__col" v-for="(item,index) in items" :key="index">{{item.title}}</th>
+          </tr>
+        </thead>
+        <tbody class="table__tbody tbody">
+          <tr class="tbody__row">
+            <td class="tbody__col" v-for="(item,index) in items" :key="index">{{item.body}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Education",
+  data() {
+    return {
+      items: [
+        { 
+          title: "نام" ,
+          body : "امیر",
+        },
+        { 
+          title: "تاریخ تولد",
+          body:  "1366" ,
+        },
+        { 
+          title: "وضیعت تاهل",
+          body:  "متاهل" ,
+        },
+        
+        { 
+          title: "وضعیت سربازی",
+          body:  "اتمام خدمت" ,
+        },
+      ],
+    };
+  },
+};
+</script>
