ede8e2ab6282035a526a4aa3e926a330bd30a566
Amirrr1987 <Maghami.A1987@gmail.com>
2020-09-17 13:23:01 +0430

    import education component in home page

--- src/layout/Home.vue
+++ src/layout/Home.vue
@@ -2,17 +2,20 @@
   <main class="home">
       <Basic/>
       <Status/>
+      <Education/>
   </main>
 </template>
 
 <script>
 import Basic from '@/components/Basic';
 import Status from '@/components/Status';
+import Education from '@/components/Education';
 export default {
     name: 'Home',
     components:{
         Basic,
         Status,
+        Education,
     }
 }
 </script>
