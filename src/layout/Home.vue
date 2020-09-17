f8ca108a4f8860feaada28d739b27997a7d8631b
Amirrr1987 <Maghami.A1987@gmail.com>
2020-09-17 13:09:36 +0430

    add status in home

--- src/layout/Home.vue
+++ src/layout/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <main class="home">
       <Basic/>
+      <Status/>
   </main>
 </template>
 
 <script>
 import Basic from '@/components/Basic';
+import Status from '@/components/Status';
 export default {
     name: 'Home',
     components:{
         Basic,
+        Status,
     }
 }
 </script>
