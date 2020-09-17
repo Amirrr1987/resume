ae71533ffaad62a750e281d6ac11c151d271280e
Amirrr1987 <Maghami.A1987@gmail.com>
2020-09-17 11:28:44 +0430

    fix & wirte BEM class in basic & imp to style.scss

--- src/components/Basic.vue
+++ src/components/Basic.vue
@@ -1,15 +1,15 @@
 <template>
   <section class="basic">
     <div class="basic__container">
-      <table class="basic__table">
-        <thead class="basic__table-head">
-          <tr class="basic__table-head-row">
-            <th class="basic__table-head-col">111</th>
+      <table class="basic__table table">
+        <thead class="table__thead thead">
+          <tr class="thead__row">
+            <th class="thead__col thead--title">111</th>
           </tr>
         </thead>
-        <tbody class="basic__table-body">
-          <tr class="basic__table-body-row">
-            <td class="basic__table-body-col">
+        <tbody class="table__tbody tbody">
+          <tr class="tbody__row">
+            <td class="tbody__col">
               111
             </td>
           </tr>
