7f7aa1fd939860dbf78f15749549d3cc87a19085
Amirrr1987 <Maghami.A1987@gmail.com>
2020-09-17 18:33:15 +0430

    create Language component

---
+++ src/components/Language.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="language">
+    <div class="language__container">
+      <table class="language__table table">
+        <thead class="table__thead thead">
+          <tr class="thead__row">
+            <th class="thead__col thead--title" colspan="4">{{title}} :</th>
+          </tr>
+          <tr class="thead__row">
+            <th class="thead__col" v-for="(item,index) in items" :key="index">{{item.title}}</th>
+          </tr>
+        </thead>
+        <tbody class="table__tbody tbody">
+          <tr class="tbody__row">
+            <td class="tbody__col" v-for="(item,index) in items" :key="index">{{item.body}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Language",
+  data() {
+    return {
+      title: "زبان های خارجی",
+      items: [
+        { 
+          title: "نام" ,
+          body : "امیر",
+        },
+        { 
+          title: "تاریخ تولد",
+          body:  "1366" ,
+        },
+        { 
+          title: "وضیعت تاهل",
+          body:  "متاهل" ,
+        },
+        
+        { 
+          title: "وضعیت سربازی",
+          body:  "اتمام خدمت" ,
+        },
+      ],
+    };
+  },
+};
+</script>
