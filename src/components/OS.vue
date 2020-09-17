6123bdb868e89dacbe70f34a9e9a88a8346b7cba
Amirrr1987 <Maghami.A1987@gmail.com>
2020-09-17 19:36:00 +0430

    add OS tags

---
+++ src/components/OS.vue
@@ -0,0 +1,49 @@
+<template>
+
+      <table class="table">
+        <thead class="table__thead thead">
+          <tr class="thead__row">
+            <th class="thead__col thead--title" colspan="4">{{title}} :</th>
+          </tr>
+          <tr class="thead__row">
+            <th class="thead__col" v-for="(item,index) in items" :key="index">{{item.title}}</th>
+          </tr>
+        </thead>
+        <tbody class="table__tbody tbody">
+          <tr class="tbody__row">
+            <td class="tbody__col" v-for="(item,index) in items" :key="index">{{item.body}}</td>
+          </tr>
+        </tbody>
+      </table>
+
+</template>
+
+<script>
+export default {
+  name: "Skill",
+  data() {
+    return {
+      title: "مهارت ها",
+      items: [
+        { 
+          title: "نام" ,
+          body : "امیر",
+        },
+        { 
+          title: "تاریخ تولد",
+          body:  "1366" ,
+        },
+        { 
+          title: "وضیعت تاهل",
+          body:  "متاهل" ,
+        },
+        
+        { 
+          title: "وضعیت سربازی",
+          body:  "اتمام خدمت" ,
+        },
+      ],
+    };
+  },
+};
+</script>
