d0f5eee5f75b79364b8b14c3c25629094457ebed
Amirrr1987 <Maghami.A1987@gmail.com>
2020-09-17 11:05:24 +0430

    create basic and include tags

---
+++ src/components/basic.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="basic">
+    <div class="basic__container">
+      <table class="basic__table">
+        <thead class="basic__table-head">
+          <tr class="basic__table-head-row">
+            <th class="basic__table-head-col">111</th>
+          </tr>
+        </thead>
+        <tbody class="basic__table-body">
+          <tr class="basic__table-body-row">
+            <td class="basic__table-body-col">
+              111
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Basic'
+}
+</script>
+
+<style>
+
+</style>
